a11b42529f2afc786750c8da5b8c61b7a5463108
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-08-13 16:21:07 +0300

    added entites, context and created tables

---
+++ StoreFlow/Context/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreFlow.Entities;
+
+namespace StoreFlow.Context
+{
+    public class StoreContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-213H3J8\\SQLEXPRESS;Database=StoreFlowDb;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
