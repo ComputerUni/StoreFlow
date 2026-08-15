78f519098ffc1c2a32488f54208d97877b9acb0c
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-08-15 22:04:04 +0300

    update: Category with ToList,Add,Update,Delete,ChangeStatus

---
+++ StoreFlow/Controllers/CategoryController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreFlow.Context;
+using StoreFlow.Entities;
+using X.PagedList.Extensions;
+
+namespace StoreFlow.Controllers
+{
+    public class CategoryController(StoreContext _context) : Controller
+    {
+        public IActionResult CategoryList(int page = 1)
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories.ToPagedList(page,8));
+        }
+
+        public IActionResult ChangeStatus(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if(category != null)
+            {
+                category.CategoryStatus = !category.CategoryStatus;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("CategoryList");
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            category.CategoryStatus = false;
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
