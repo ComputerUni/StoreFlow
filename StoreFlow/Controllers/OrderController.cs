6efe8d25ed6e73fefbed51407fd538c02e6ab812
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-08-16 11:46:51 +0300

    feat: implement async order operations with task, route and CRUD fixes

--- StoreFlow/Controllers/OrderController.cs
+++ StoreFlow/Controllers/OrderController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using StoreFlow.Context;
+using StoreFlow.Entities;
+using X.PagedList.Extensions;
 
 namespace StoreFlow.Controllers
 {
@@ -9,7 +12,7 @@ namespace StoreFlow.Controllers
         public IActionResult AllStockSmallerThen5()
         {
             bool orderStockCount = _context.Orders.All(x => x.OrderCount < 5);
-            if(orderStockCount)
+            if (orderStockCount)
             {
                 ViewBag.v = "Tüm siparişler 5 adetten küçüktür.";
             }
@@ -23,12 +26,68 @@ namespace StoreFlow.Controllers
         public IActionResult OrderListByStatus(string status)
         {
             var values = _context.Orders.Include(x => x.Customer).Include(x => x.Product).Where(x => x.Status.Contains(status)).ToList();
-            if(!values.Any())
+            if (!values.Any())
             {
                 ViewBag.v = "Bu status ile ilgili veri bulunamadı.";
             }
 
             return View(values);
+        }
+
+        public IActionResult OrderListSearch(string name, string filterType)
+        {
+            if (filterType == "start")
+            {
+                var values = _context.Orders.Include(x => x.Customer).Include(x => x.Product).Where(x => x.Status.StartsWith(name)).ToList();
+                return View(values);
+            }
+            else if (filterType == "end")
+            {
+                var values = _context.Orders.Include(x => x.Customer).Include(x => x.Product).Where(x => x.Status.EndsWith(name)).ToList();
+                return View(values);
+            }
+
+            var orders = _context.Orders.Include(x => x.Customer).Include(x => x.Product).ToList();
+
+            return View(orders);
+        }
+
+        public async Task<IActionResult> OrderListAsync2(int page = 1)
+        {
+            var values = await _context.Orders.Include(x => x.Product).Include(y => y.Customer).ToListAsync();
+            return View(values.ToPagedList(page, 8));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateOrder()
+        {
+            var products = await _context.Products.Select(p => new SelectListItem
+            {
+                Value = p.ProductId.ToString(),
+                Text = p.ProductName
+            }).ToListAsync();
+
+            ViewBag.products = products;
+
+            var customers = await _context.Customers.Select(c => new SelectListItem
+            {
+                Value = c.CustomerId.ToString(),
+                Text = c.CustomerName + " " + c.CustomerSurname
+            }).ToListAsync();
+
+            ViewBag.customers = customers;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(Order order)
+        {
+            order.Status = "Sipariş Alındı";
+            order.OrderDate = DateTime.Now;
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("OrderListAsync2");
         }
     }
 }
--- StoreFlow/obj/Debug/net9.0/StoreFlow.AssemblyInfo.cs
+++ StoreFlow/obj/Debug/net9.0/StoreFlow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365c87672e8c3b5800bf8d7ef09d9283e01e7cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0915f917b17d5a02ac7c2a2229b405f3833f934b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
